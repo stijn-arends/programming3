4736b3833855c6b977c9a3fb4403f95e73ae1a3c
stijn-arends <stijnarends@live.nl>
2022-05-14 18:43:41 +0200

    Added a command line argument parser class.

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,100 +1,93 @@
+import sys
 from Bio import Entrez
 from bs4 import BeautifulSoup
 import multiprocessing as mp
+import argparse
+import os
 
-# Assignment: https://bioinf.nl/~martijn/master/programming2/assignment1.html
+# Assignment: https://bioinf.nl/~martijn/master/programming3/assignment1.html
 # Entrenz documentation: http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec143
 
 
 Entrez.email = "stijnarends@live.nl"  # Always tell NCBI who you are
-handle = Entrez.einfo()
-print(handle)
-#result = handle.read()
-# handle.close()
-
- 
-# Passing the stored data inside
-# the beautifulsoup parser, storing
-# the returned object
-# Bs_data = BeautifulSoup(result, "xml")
- 
-# Finding all instances of tag
-# `unique`
-# b_unique = Bs_data.find_all('DbName')
-
- 
-# print(b_unique)
 
 
-# ----
-# Use entrenz.efetch(pmid) to get the paper in xml format.
-# ----
+__author__ = "Stijn Arends"
+__version__ = "v0.1"
+__data__ = "14-5-2022"
 
 
-def retrieve_ref_ids(pmid):
-    ids = []
-    record = Entrez.read(Entrez.elink(dbfrom="pubmed", id=pmid))
+class ArgumentParser:
+    """
+    Class to parse the input arguments.
+    """
 
+    def __init__(self):
+        parser = self._create_argument_parser()
+        # Print help if no arguments are supplied and stop the program
+        if len(sys.argv) == 1:
+            parser.print_help(sys.stderr)
+            sys.exit(1)
+        self.arguments = parser.parse_args()
 
-    # Get the references of the paper
-    index = 5
-    for i, rec in enumerate(record[0]["LinkSetDb"]):
-        # print(rec)
-        if  rec['LinkName'] == "pubmed_pubmed_refs":
-            index = i
+    @staticmethod
+    def _create_argument_parser():
+        """
+        Create an argument parser.
+        
+        :arguments
+        ----------
+        -pmid - pubmed id
+        -v, --version - displays the version of the script
+        -h, --help - display the help text
+
+        :returns
+        --------
+        parser - ArgumentParser
+        """
+        parser = argparse.ArgumentParser(prog=os.path.basename(__file__),
+            description="Python script that downloads 10 papers from the reference section from an article in pubmed.",
+            epilog="Contact: stijnarend@live.nl")
+
+        # Set version
+        parser.version = __version__
+
+        parser.add_argument('-pmid',
+            required=True,
+            help='Pubmed ID')
+
+        parser.add_argument('-v',
+            '--version',
+            help='Displays the version number of the script and exitst',
+            action='version')
+
+        return parser
+
+    def get_argument(self, argument_key):
+        """
+        Method to get an input argument.
+        :parameters
+        -----------
+        argument_key - str 
+            Full command line argument (so --config for the configuration file argument).
+
     
-    for link in record[0]["LinkSetDb"][index]["Link"]:
-        ids.append(link["Id"])
-
-    return ids
-
-
-def fetch_paper(pmid):
-    paper = Entrez.efetch(db="pubmed", id=pmid, retmode='xml').read() # retmode='txt', rettype='xml'
-    write_xml(paper, pmid)
-
-
-def write_xml(data, name):
-    bs_data = BeautifulSoup(data, "xml")
-
-    f = open(f'{name}.xml', "w", encoding="utf-8")
-    f.write(bs_data.prettify())
-    f.close()
-
+        :returns
+        --------
+        value - List or boolean
+        """
+        if self.arguments is not None and argument_key in self.arguments:
+            value = getattr(self.arguments, argument_key)
+        else:
+            value = None
+        return value
 
 def main():
-    # pmid = "35223391" # "19304878" # "33669327" # 
-    pmid = "19304878"
-    # results = Entrez.read(Entrez.elink(dbfrom="pubmed", db="pmc", LinkName="pubmed_pmc_refs", id=pmid))
-    # pmc_ids = [link["Id"] for link in results[0]["LinkSetDb"][0]["Link"]]
-    # print(pmc_ids)
-    # print(len(pmc_ids))
+    # Get passed arguments
+    cla_parser = ArgumentParser()
+    pmid = cla_parser.get_argument('pmid')
 
-
-    ids = retrieve_ref_ids(pmid)
-
-    print(ids)
-    print(len(ids))
-
-    test_id = ids[1]
-    print(test_id)
-
-    # paper = Entrez.efetch(db="pubmed", id=test_id, retmode='xml').read()
-
-    # bs_data = BeautifulSoup(paper, "xml")
-
-    # print(str(bs_data))
-
-    # f = open(f'{test_id}.xml', "w", encoding="utf-8")
-    # f.write(bs_data.prettify())
-    # f.close()
-
-    # cpus = mp.cpu_count()
-
-    # with mp.Pool(4) as pool:
-    #     pool.map(fetch_paper, ids[:10])
-    for id in ids[:10]:
-        fetch_paper(id)
+    print(f"Pubmed ID: {pmid}")
 
 if __name__ == "__main__":
     main()
