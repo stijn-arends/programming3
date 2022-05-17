d93a5f3d240bd2081f027a32ec5aaeaad799e771
stijn-arends <stijnarends@live.nl>
2022-05-17 20:28:08 +0200

    Created a method that can write out data to a pickle file

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -1,11 +1,12 @@
 import sys
 from Bio import Entrez
 import multiprocessing as mp
-import argparse
-import os
+from multiprocessing.managers import BaseManager, SyncManager
+import time, queue, os
 from pathlib import Path
 from typing import Any
 from bs4 import BeautifulSoup
+import pickle
 
 from arg_parser import ArgumentParser
 
@@ -39,6 +40,22 @@ class DownloadAuthorList:
     def __init__(self, n_articles:int, out_path: Path) -> None:
         self.n_articles = n_articles
         self.out_path = out_path
+        self.make_data_dir(out_path)
+
+    def make_data_dir(self, path: Path) -> None:
+        """
+        Create a directory (if it does not exsit yet) to store the 
+        data.
+
+        :Excepts
+        --------
+        FileExistsError
+            The directory already exists
+        """
+        try:
+            path.mkdir(parents=True, exist_ok=False)
+        except FileExistsError:
+            print(f"[{self.make_data_dir.__name__}] {path} already exists.")
 
     def get_id_references(self, pmid) -> list:
         """
@@ -72,10 +89,10 @@ class DownloadAuthorList:
         """
         print(f"Downloading paper: {pmid}")
         paper = Entrez.efetch(db="pmc", id=pmid, rettype="XML", retmode="text").read()
-        self.get_author_names(paper)
+        self.get_author_names(paper, pmid)
 
 
-    def get_author_names(self, paper:bytes) -> None:
+    def get_author_names(self, paper:bytes, pmid:int) -> None:
         """
         Get the names of the authors of a paper.
 
@@ -91,12 +108,50 @@ class DownloadAuthorList:
             name = " ".join(child.text for child in author_info.findChildren())
             authors.append(name)
 
-        print(f"Authors: {authors}")
+        # print(f"Authors: {tuple(authors)}")
+
+        output_file = self.out_path / f"{pmid}.authors.pickle"
+        self.write_pickle(tuple(authors), output_file)
+
+    @staticmethod
+    def write_pickle(data: Any, file: Path) -> None:
+        """
+        Write data to a pickle file
+
+        :parameters
+        -----------
+        data - Any
+            Any data that can be pickled
+        file - Path
+            Name and location of the file to write to
+        """
+        with open(file, 'wb') as fh:
+            pickle.dump(data, fh)
 
 
 def main():
     # Get passed arguments
     cla_parser = ArgumentParser()
+    pmid = cla_parser.get_argument('pubmed_id')
+    n_articles= cla_parser.get_argument('a')
+
+    n_peons = cla_parser.get_argument('n')
+    port = cla_parser.get_argument('p')
+    host = cla_parser.get_argument('host')
+
+    print(f"Pubmed ID: {pmid}, articles to download: {n_articles}")
+    print(f"Number of peons: {n_peons}")
+    print(f"Port number: {port}")
+    print(f"host: {host}")
+    out_path = Path(os.path.abspath(os.path.dirname(__file__))) / "output"
+    download_auths = DownloadAuthorList(n_articles=n_articles, out_path=out_path)
+
+    ref_ids = download_auths.get_id_references(pmid)
+
+    print(f"Reference IDs: {ref_ids}")
+
+    download_auths.download_paper(ref_ids[0])
+
 
 if __name__ == "__main__":
     main() # 30049270
