9b6322f74d2c8b9db729f8fc2b0f247a8c8f4bdb
stijn-arends <stijnarends@live.nl>
2022-05-26 15:34:18 +0200

    when calling the python script also specify the name of the output image file.

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=stijnarends@live.nl
-#SBATCH --time 2:00:00
+#SBATCH --time 4:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=BlastpStijn
@@ -16,14 +16,12 @@ echo $script_relative_dir
 
 # Output 
 out_folder=output # ${script_relative_dir}/
-out_time=${out_folder}/timings.txt
+out_time=${out_folder}/timings
 out_blast=blastoutput.txt
 
 # Input
 query_file=MCRA.faa
 export BLASTDB=/local-fs/datasets/
-
-echo "Output folder: ${out_folder}"
 
 if [ ! -f "${query_file}" ]; then
     echo "Invalid query file. File ${query_file} does not exist. Make sure that you are in the correct directory."
@@ -39,7 +37,7 @@ fi
 for i in $( seq 1 $cpus )
 do
     echo "Iteration: $i"
-    /usr/bin/time -a --output ${out_time} -f "$i %e" blastp -query ${query_file} -db refseq_protein/refseq_protein -num_threads $i -outfmt 6 >> ${out_blast}
+    /usr/bin/time -a --output ${out_time}.txt -f "$i %e" blastp -query ${query_file} -db refseq_protein/refseq_protein -num_threads $i -outfmt 6 >> ${out_blast}
 done
 
-python3 plot_time_benefit.py ${out_time}
+python3 plot_time_benefit.py "${out_time}.txt" -o "${out_time}.png"
