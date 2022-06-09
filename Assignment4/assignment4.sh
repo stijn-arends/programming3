421f9d9fa0351fd81c13c8cc19587989a387debe
stijn-arends <stijnarends@live.nl>
2022-06-09 15:57:45 +0200

    Fixed output issue

--- Assignment4/assignment4.sh
+++ Assignment4/assignment4.sh
@@ -3,42 +3,26 @@
 forward=/data/dataprocessing/MinIONData/MG5267/MG5267_TGACCA_L008_R1_001_BC24EVACXX.filt.fastq 
 reverse=/data/dataprocessing/MinIONData/MG5267/MG5267_TGACCA_L008_R2_001_BC24EVACXX.filt.fastq 
 
-out_folder=/commons/dsls/dsph/2022/velvet_out_run_full/output
+out_folder=/commons/dsls/dsph/2022/velvet_out_run
 
 mkdir -p ${out_folder}
 
 # Output destination
-output=${out_folder}/kmr_
+output_kmr=${out_folder}/kmr_ # output=${out_folder}/kmr_
 
-kmr_size_N50=${out_folder}/kmr_size_N50.csv
+# Final output
+output=output
+mkdir -p ${output}
+kmr_size_N50=${output}/kmr_size_N50.csv # ${out_folder}/kmr_size_N50.csv
 
+seq 31 2 31 | parallel -j16 "velveth ${output_kmr}{} {} -longPaired -fastq -separate ${forward} ${reverse} && velvetg ${output_kmr}{} && cat ${output_kmr}{}/contigs.fa | python3 assignment4.py -kmr {} >> ${kmr_size_N50}"
 
-# Works
-# -------
-
-# seq 27 2 31 | parallel -j16 velveth $output{} {} -longPaired -fastq -separate $forward $reverse
-
-# seq 27 2 31 | parallel velvetg $output{}
-
-# Best option
-seq 5 2 31 | parallel -j16 "velveth ${output}{} {} -longPaired -fastq -separate ${forward} ${reverse} && velvetg ${output}{} && cat ${output}{}/contigs.fa | python3 assignment4.py -kmr {} >> ${kmr_size_N50}"
-
-
-# Hardcoded paths
-# seq 27 2 31 | parallel -j16 'velveth /commons/dsls/dsph/2022/test2/Stijn_{} {} -longPaired -fastq -separate /data/dataprocessing/MinIONData/MG5267/MG5267_TGACCA_L008_R1_001_BC24EVACXX.filt.fastq /data/dataprocessing/MinIONData/MG5267/MG5267_TGACCA_L008_R2_001_BC24EVACXX.filt.fastq && velvetg /commons/dsls/dsph/2022/test2/Stijn_{}'
-
-# -------
-
-# Find the best kmr size
-# ----------------
 highest_N50=0
 highest_kmr=1
 
 while IFS=, read -r kmr n50
 do
-    # echo "I got:$kmr|$n50"
     if [ "$n50" -gt "$highest_N50" ]; then
-    # echo "${n50} > ${highest_N50}"
     highest_N50=${n50}
     highest_kmr=${kmr}
     fi
@@ -47,10 +31,10 @@ done < ${kmr_size_N50}
 echo "The highest kmr is ${highest_kmr} with an N50 of: ${highest_N50}"
 
 # Move contigs.fa to the output folder
-mv ${output}${highest_kmr}/contigs.fa ${out_folder}
+mv ${output_kmr}${highest_kmr}/contigs.fa ${output}
 
 
-vals=($(seq 5 2 31))
+vals=($(seq 31 2 31))
 
 for kmr in ${vals[@]}
 do
@@ -60,5 +44,5 @@ do
     #     echo "Remove: ${kmr}"
     #     rm -r ${output}${kmr}
     # fi
-    rm -r ${output}${kmr}
+    rm -r ${output_kmr}${kmr}
 done
