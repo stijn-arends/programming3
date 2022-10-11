84b18b05aa7282e2f5dd6a3fa715010d87ab4c7f
stijn-arends <stijnarends@live.nl>
2022-10-11 16:46:18 +0200

    Added methods to the PubmedParser class.
    For instance:
    	- method to get publish date
    	- method to get references
    		- There are two methods that can process two different
    		  ways of citing an article.

--- Assignment6/parse_pubmed_xml.py
+++ Assignment6/parse_pubmed_xml.py
@@ -281,6 +281,18 @@ class PubmedParser:
 
     :get_publish_date
     :get_references
+
+    To-do:
+        - Think about using the __iter__ method instead of the
+        parse_articles method.
+        Inspiration mcoding video:
+        https://www.youtube.com/watch?v=tmeKsb2Fras
+        Look at prog2 exercises for more inspiration.
+        - Decide which reference type you want
+            - all authors
+            - only main author
+        Only the main author should be sufficient for coupling authors
+        to the PMID after everything has been parsed. 
     """
 
     def __init__(self, xml_file: Path) -> None:
@@ -328,10 +340,19 @@ class PubmedParser:
     def parse_articles(self):
         """
         Create a dictionary of lists and save the results in here.
+
+        for file or file_1:
+            * article 24:25 has a PMC
+            * article 47:48 has elocationID
+
+        For the 'file_2':
+            * article 0:1 has no ReferenceList
+            * article 12:13 has a citation, articleIdList reference
+            * article 5403:5404 has a citation reference
         """
         # file 47:48 has elocationID
         # file 24:25 has a PMC
-        for article in self.data[24:25]:
+        for article in self.data[12:13]:
             # print(article)
             medline_parser = MedlineParser(article)
             pmid = medline_parser.get_pmid()
@@ -352,6 +373,215 @@ class PubmedParser:
             print(f"DOI: {doi}")
             pmc = medline_parser.get_pmc()
             print(f"PMC: {pmc}")
+            pubblish_date = self.get_publish_date(article)
+            print(f"Publish date: {pubblish_date}")
+
+            ref_id_list = self.get_references(article)
+            print(f"Reference ID list: {ref_id_list}")
+
+    @staticmethod
+    def get_publish_date(article: dict) -> datetime.date:
+        """
+        Get the date of publishing the article in year-month-day format.
+
+        :parameters
+        -----------
+        article - dict
+            The article inside a dictionary.
+
+        :returns
+        --------
+        date - datetime.date
+            The date of publishing, returns empty str when date can't be found.
+        """
+        try:
+            year = int(article['PubmedData']['History'][0]['Year'])
+            month = int(article['PubmedData']['History'][0]['Month'])
+            day = int(article['PubmedData']['History'][0]['Day'])
+            date = datetime.date(year, month, day)
+        except IndexError:
+            date = ""
+        return date
+
+    def get_references(self, article: dict) -> list[Any]:
+        """
+        Extract either the main author or the PMID of all the references.
+        First look if the article contains references. Next, check which reference 
+        style was used:
+
+        1.	A dictionary with 2 keys, “citation” and “ArticleIdList”. The citation has as value:
+            journal + journal edition, and ArticleIdList has as value: a string representation
+            of the PMID.
+
+            Example:
+                {'Citation': 'J Biol Chem. 1958 Nov;233(5):1135-9', 'ArticleIdList':
+                [StringElement('13598746', attributes={'IdType': 'pubmed'})]}
+
+        2.	A dictionary with 1 key: “citation”, which has a value containing:
+            authors + year + title + journal edition
+
+            Example:
+                {'Citation': 'Adam, E.J.H. and Adam, S.A. 1994. Identification of
+                cytosolic factors required for nuclear location sequence-mediated
+                binding to the nuclear envelope. J. Cell Biol. 125:547-555.'}
+
+        :parameters
+        -----------
+        article - dict
+            The article inside a dictionary.
+
+        :returns
+        --------
+        ref_id_list - list[str]
+            List of either the PMID of the references or the authors.
+
+        :see also
+        ---------
+        PubmedParser._get_pmid_reference()
+        PubmedParser._get_authors_reference()
+        PubmedParser._get_main_author_references()
+        """
+        # print(article['PubmedData']['ReferenceList'])
+        try:
+            reference_list = article['PubmedData']['ReferenceList'][0]
+            #  print(f"references: {len(reference_list)}")
+
+            references = reference_list['Reference']
+            if len(references[0].keys()) > 1:
+                # print(f"\nTest: {references[0]['ArticleIdList'][0].attributes}")
+                # print(len(references))
+                ref_id_list = self._get_pmid_reference(references)
+
+                # print(f"PMID list: {ref_id_list}")
+                # print(len(ref_id_list))
+            else:
+                # ref_id_list = self._get_authors_reference(references)
+                # print(f"Authors list references: {ref_id_list}\n")
+                ref_id_list = self._get_main_author_references(references)
+                # print(f"Main authors list references: {ref_id_list}\n")
+        except IndexError:
+            ref_id_list = []
+
+        return ref_id_list
+
+    @staticmethod
+    def _get_pmid_reference(references: list) -> list[int]:
+        """
+        Get the PMID from the ArticleIdList for each reference.
+
+        :parameters
+        -----------
+        reference - list
+            List of the references.
+
+        :returns
+        --------
+        pmid_list - list[str]
+            List of the PMID from the references.
+        """
+        pmid_list = []
+        for reference in references:
+            # print(reference)
+            # print(reference["ArticleIdList"][0])
+            article_id_list = reference["ArticleIdList"][0]
+            pmid = int(article_id_list) if article_id_list.attributes["IdType"] == "pubmed" else ""
+            # print(f"PMID: {pmid}")
+            pmid_list.append(pmid)
+        
+        return pmid_list
+
+    @staticmethod
+    def _get_authors_reference(references: list) -> list[str]:
+        """
+        Get the names of the authors for each reference using regex. Extract all the information
+        before the first number is found (i.e. year of publishing). Next, split that text on '. '
+        and if that did not work split it on ', ' to seperate the different authors. If this is the
+        case then combine the sir name and initials and put them into a new list. 
+        
+        :parameters
+        -----------
+        reference - list
+            List of the references.
+
+        :returns
+        --------
+        authors_references - list[str]
+            List of the authors from the references.
+
+        Format of reference: 
+            * authors + year + title + journal edition / book
+
+        Example:
+            'Adam, E.J.H. and Adam, S.A. 1994.
+            Identification of cytosolic factors required for nuclear location sequence-mediated
+            binding to the nuclear envelope. J. Cell Biol. 125:547-555.'
+        """
+        pattern = r'^(.+?)(?=\d).*'
+        authors_references = []
+        try: 
+            for reference in references:
+                check = re.search(pattern, reference["Citation"])
+                names = check.group(1)
+                
+                names = names.replace('and ', '').rstrip()
+                names = names.split('. ')
+                correct_names = []
+                if len(names) == 1:
+                    names = names[0].split(', ')
+                    for i in range(1, len(names), 2):
+                        correct_names.append(names[i -1] + ", " + names[i])
+
+                    authors_references.append(correct_names)
+                else:
+                    authors_references.append(names)
+        except Exception:
+            pass
+
+        # print(f"Authors of the references: {authors_references}")
+        return authors_references
+
+    @staticmethod
+    def _get_main_author_references(references: list) -> list[str]:
+        """
+        Get only the main author listed inside the reference using regex.
+
+        :parameters
+        -----------
+        reference - list
+            List of the references.
+
+        :returns
+        --------
+        main_author_references - list[str]
+            List of the main authors from the references.
+
+        Format of reference: 
+            * authors + year + title + journal edition / book
+
+        Example:
+            'Adam, E.J.H. and Adam, S.A. 1994.
+            Identification of cytosolic factors required for nuclear location sequence-mediated
+            binding to the nuclear envelope. J. Cell Biol. 125:547-555.'
+
+        Try to catch all the sir name + initials:
+            * https://regex101.com/r/XhU4Ak/1
+            * pattern = r'([ÄÖÜäöüßA-Za-z,\s])+(?:[A-Z]\.){1,10}'
+        """
+        pattern = r'([ÄÖÜäöüßA-Za-z,\s])+(?:[A-Z]\.){1,10}'
+        test_string = 'Adam, E.J.H. and Adam, S.A. 1994. Identification of cytosolic factors required for nuclear location sequence-mediated binding to the nuclear envelope. J. Cell Biol. 125:547-555.'
+        # match = re.search(pattern, test_string)
+        # print(f"Match: {match}")
+        # print(f"Group: {match.group(0)}")
+        main_author_references = []
+        try:
+            for reference in references:
+                match = re.search(pattern, reference["Citation"])
+                # print(f"Main author: {match.group(0)}")
+                main_author_references.append(match.group(0))
+        except Exception:
+            pass
+
+        return main_author_references
 
 
 def main():
@@ -372,9 +602,9 @@ def main():
     # ---- test  1 -----
 
     # medline_parser = MedlineParser()
-    parser = PubmedParser(file)
+    parser = PubmedParser(file_2)
 
-    records = parser.get_parsed_xml()
+    # records = parser.get_parsed_xml()
 
     parser.parse_articles()
 
