9d9a6f2a4b31da9011c29148038527fef9d0824f
stijn-arends <stijnarends@live.nl>
2022-10-23 18:42:37 +0200

    Changed filetype results are saved to.
    Used to be csv files but changed to json files in order to save the
    datatypes of the dataframe. This will make it easier to use and analyze the parsed
    data.

--- Assignment6/src/main.py
+++ Assignment6/src/main.py
@@ -9,7 +9,7 @@ __version__ = "v.01"
 from pathlib import Path
 import time
 import multiprocessing as mp
-from datetime import datetime as dt
+import json
 
 from arg_parser import ArgumentParser, CLIArgValidator
 from server_side import ServerSide
@@ -30,8 +30,12 @@ def parse_file(file: Path, out_dir: Path) -> None:
     """
     parser = PubmedParser(file)
     df = parser.parse_articles()
-    out_file = out_dir / (file.stem + ".csv")
-    df.to_csv(out_file, sep="\t", index=False, header=True)
+    out_file = out_dir / (file.stem + ".json")
+    json_data = json.loads(df.to_json(orient='records', date_format='iso'))
+
+    with open(out_file, 'w') as f:
+        f.write(json.dumps(json_data))
+
 
 
 def make_output_dir(path: Path) -> None:
@@ -84,13 +88,11 @@ def main() -> None:
     # ---- Test server client mode ------
 
     if server_mode:
-        # out_dir = Path("/commons/dsls/dsph/2022/test")
-        # out_dir.mkdir(exist_ok=True)
         server_side = ServerSide(ip_adress=host, port=port,
             auth_key=b'whathasitgotinitspocketsesss?',
             poison_pill="MEMENTOMORI")
         server = mp.Process(target=server_side.run_server, args=(parse_file,
-            data_files[:2], output_dir))
+            data_files, output_dir))
         server.start()
         time.sleep(1)
         server.join()
@@ -111,64 +113,6 @@ def main() -> None:
     elapsed = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
     print(f"\n---- Time: {days}:{elapsed} ----")
     # ----- End server client mode ------
-
-
-    # data_dir = Path("/data/dataprocessing/NCBI/PubMed/")
-
-    # This contains one option for referecing using PMID
-    # file = "/data/dataprocessing/NCBI/PubMed/pubmed21n0455.xml"
-
-    # This contains the other option for referencing using AUthor ID
-    file_2 = "/data/dataprocessing/NCBI/PubMed/pubmed21n0591.xml"
-
-    # Contains empty reference
-    file_3 = "/data/dataprocessing/NCBI/PubMed/pubmed21n0591.xml"
-
-    # File that contains a date error - problem solved
-    file_4 = "/data/dataprocessing/NCBI/PubMed/pubmed21n0379.xml"
-
-    # File that contains an error that occurs in _get_main_author_refences - no longer using this function
-    file_5 = "/data/dataprocessing/NCBI/PubMed/pubmed21n0632.xml"
-
-    # File that contains an error whilst trying to get the pmid reference - problem solved
-    file_6 = "/data/dataprocessing/NCBI/PubMed/pubmed21n1049.xml"
-
-    # Another datetime error in both files - year is equal to 1 which causes an outofdatebound error.
-    file_7 = "/data/dataprocessing/NCBI/PubMed/pubmed21n0542.xml"
-    file_8 = "/data/dataprocessing/NCBI/PubMed/pubmed21n0943.xml"
-    # pd.Timestamp('15000101')
-
-    # ---- test  1 -----
-
-    # parser = PubmedParser(file_8)
-
-    # # records = parser.get_parsed_xml()
-
-    # df = parser.parse_articles()
-
-    # print(df.head(10))
-
-    # # print(df.ref_titles)
-
-    # title_refs = df[df["ref_titles"].str.len() != 0]
-    # print(title_refs)
-
-    # example_list = title_refs.iloc[0, -2]
-    # print(f"Example list: {example_list}")
-    # print(type(example_list))
-
-    # ----- test all files ----
-
-    # out_dir = Path("/commons/dsls/dsph/2022/parsed_pubmed_articles")
-    # out_dir.mkdir(exist_ok=True)
-
-    # for i, file in enumerate(data_dir.glob("*.xml")):
-    #     if i > 941:
-    #         print(f"Parsing file: n: {i+1}, name: {file.stem}")
-    #         parser = PubmedParser(file)
-    #         df = parser.parse_articles()
-    #         out_file = out_dir / (file.stem + ".csv")
-    #         df.to_csv(out_file, sep="\t", index=False, header=True)
 
 
 if __name__ == "__main__":
